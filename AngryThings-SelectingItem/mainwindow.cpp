cd4b891d82c142bc437e394fb58406414ae6c381
kevinbird61 <kevinbird61@gmail.com>
2016-03-30 15:53:09 +0800

    Add the selecting item page prototype.

--- AngryThings-SelectingItem/mainwindow.cpp
+++ AngryThings-SelectingItem/mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
 
-MainWindow::MainWindow(QWidget *parent) :
+using namespace std;
+
+MainWindow::MainWindow(qreal width, qreal height, QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    x_sceneSize = width-200;
+    y_sceneSize = height-100;
     ui->setupUi(this);
+    scene = new QGraphicsScene(0,0,x_sceneSize,y_sceneSize);
+    ui->graphicsView->setScene(scene);
+    ui->graphicsView->show();
 }
 
 MainWindow::~MainWindow()
