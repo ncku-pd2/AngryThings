b5f8acc6c94ce9748841daeaa0f6850afd567715
LuisHsu <luis8404272@gmail.com>
2016-04-08 19:48:56 +0800

    Wrap main

--- AngryThing/main.cpp
+++ AngryThing/main.cpp
@@ -5,23 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    // ================= Calculate the QMainWindow =================
-    QDesktopWidget wid;
-    int screenWidth = wid.screen()->width();
-    int screenHeight = wid.screen()->height();
-    int width = screenWidth - 300;
-    int height = screenHeight - 150;
-    MainWindow w(width-200, height-100);
-    // ================= Setting Windows Title and Icon =================
-    w.setWindowTitle("AngryThings");
-    w.setWindowIcon(QIcon("../angrything_img_src/object/Normal_Bird_1.png"));
-    // ================= Calculate for frame size =================
-    w.setFixedSize(width , height);
-    w.settingBackground(width-200,height);
-    w.settingInitObj();
-    // set frame size
-    w.setGeometry((screenWidth/2)-(width/2) , (screenHeight/2) - (height/2) , width , height);
+	MainWindow w(QDesktopWidget().screen()->width()-200, QDesktopWidget().screen()->height()-200);
     w.show();
-    // Program Loop
     return a.exec();
 }
